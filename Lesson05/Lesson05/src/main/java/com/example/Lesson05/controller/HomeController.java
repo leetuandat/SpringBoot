1ab37db1d8ebf5769fe4576e3c3dd2209d4d008a
leetuandat <tuandat24034@gmail.com>
2025-03-11 03:06:34 +0700

    Lesson05

---
+++ Lesson05/Lesson05/src/main/java/com/example/Lesson05/controller/HomeController.java
@@ -0,0 +1,44 @@
+/**
+ * @author X.e.n.g
+ * @version 1.O
+ * @project name: Lesson05
+ * @date: 3/4/2025
+ * @time: 07:46 PM
+ * @package: com.example.Lesson05.controller
+ */
+
+package com.example.Lesson05.controller;
+
+import com.example.Lesson05.entity.Info;
+import lombok.Generated;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+   @GetMapping("/profile")
+   public String profile(Model model) {
+        List<Info> profile = new ArrayList<>();
+        profile.add(new Info("Dat Le", "xeng", "datle24034@gmail.com", "https://www.facebook.com/atle.631578/"));
+        profile.add(new Info("Anh Le", "gacon", "unin310102@gmail.com", "https://www.facebook.com/iuck.631578/"));
+        model.addAttribute("DatProfile", profile);
+        return "profile";
+   }
+
+   @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("title", "Xeng:Home");
+        return "home";
+   }
+}
